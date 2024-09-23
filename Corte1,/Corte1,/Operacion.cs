b0aa129772e59ffa68a7f5be1896f4e36e2df84a
michaelguapisimo <mramirezc@uamv.edu.ni>
2024-09-23 10:01:31 -0600

    asd

---
+++ Corte1,/Corte1,/Operacion.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Corte1_
+{
+    internal class Operacion
+    {
+        public int CalcularEdad(Persona persona)
+        {
+            int edad = DateTime.Now.Year - persona.fechadenacimiento.Year;
+            if (DateTime.Now.DayOfYear < persona.fechadenacimiento.DayOfYear)
+            {
+                edad--;
+            }
+            return edad;
+        }
+
+        public bool EsMayorDeEdad(Persona persona)
+        {
+            return CalcularEdad(persona) >= 18;
+        }
+    }
+}
---
+++ Corte1,/Corte1,/Persona.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Corte1_
+{
+    internal class Persona
+    {
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+        public DateTime fechadenacimiento { get; set; }
+        public string ciudad { get; set; }
+    }
+}
